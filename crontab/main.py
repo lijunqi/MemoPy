226adc65c0dcb16156300574c4aaad257760eef6
lijunqi <lijunqi001@163.com>
2025-02-16 09:54:36 +0800

    update cron

--- crontab/main.py
+++ crontab/main.py
@@ -1,18 +1,24 @@
 from crontab import CronTab
+
+from cron_descriptor import get_description, ExpressionDescriptor
+
 from datetime import datetime, timezone, timedelta
+
+
+def cal_cron(expr):
+    entry = CronTab(expr)
+    now = datetime.now(timezone.utc)
+    delay_s = entry.next(default_utc=True)
+    target_t = now + timedelta(seconds=delay_s)
+    print("====== ", get_description(expr))
+    print("Now   : ", now)
+    print("Delay : ", delay_s)
+    print("Target: ", target_t)
 
 
 if __name__ == '__main__':
     # At every 5th minute from 0 through 59.
-    entry = CronTab('0/5 * * * * *')
-    now = datetime.now(timezone.utc)
-    print(now, entry.next(default_utc=False))
+    cal_cron('0/5 * * * * *')
 
     # At 08:00 on every day-of-week from Monday through Friday.
-    entry = CronTab('0 8 * * 1-5')
-    now = datetime.now(timezone.utc)
-    delay_s = entry.next(default_utc=True)
-    target_t = now + timedelta(seconds=delay_s)
-    print("Now   : ", now)
-    print("Delay : ", delay_s)
-    print("Target: ", target_t)
+    cal_cron('0 8 * * 1-5')
