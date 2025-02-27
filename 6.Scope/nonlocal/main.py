f154552fb0e47ac2fde523474455959290bf549f
lijunqi <lijunqi001@163.com>
2025-02-27 22:06:19 +0800

    scope

--- 6.Scope/nonlocal/main.py
+++ 6.Scope/nonlocal/main.py
@@ -1,5 +1,9 @@
 """
 We have to explicitly tell Python we are modifying a nonlocal variable.
+
+Whenever Python is told that a variable is nonlocal
+  * it will look for it in the enclosing local scopes chain until it first encounters the specified vairable name
+  * Beware: It will only look in local scopes, it will NOT look in the global scope
 """
 
 def outer_func():
@@ -13,3 +17,34 @@ def outer_func():
     print('x = ', x)
 
 outer_func() # world
+
+
+def outer():
+    x = 'hello'
+    def inner1():
+        def inner2():
+            nonlocal x
+            x = 'python'
+        inner2()
+
+    inner1()
+    print(x) # python
+
+outer()
+
+
+def outer_2():
+    x = 'hello'
+    def inner1():
+        x = 'python'
+        def inner2():
+            nonlocal x
+            x = 'monty'
+        print('inner(before), x = ', x) # python
+        inner2()
+        print('inner(after ), x = ', x) # monty
+
+    inner1()
+    print('outer_2, x = ', x) # hello
+
+outer_2()
