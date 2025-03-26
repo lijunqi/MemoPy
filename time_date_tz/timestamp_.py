dd5a5006265af28a90cd11d9f798f049b1a6af28
lijunqi <lijunqi001@163.com>
2025-03-26 22:32:16 +0800

    update

---
+++ time_date_tz/timestamp_.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+now = datetime.now()
+print("Local Now: ", now)
+ts = now.timestamp()
+print("Timestamp: ", ts, type(ts))
+
+date = datetime.fromtimestamp(ts)
+print("date(loc): ", date, type(date))
+
+date = datetime.fromtimestamp(ts)
+print("date(utc): ", date)
