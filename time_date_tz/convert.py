bc501702435efa288c96507d8d3b4de3767fa778
Jacky Junqi Li <jli21@rockwellautomation.com>
2025-03-27 09:20:05 +0800

    astimezone

--- time_date_tz/convert.py
+++ time_date_tz/convert.py
@@ -5,6 +5,7 @@ from datetime import datetime
 print("====== str to datetime ======")
 iso_8601_str = '2011-11-04T00:05:23+04:00'
 print("ISO8601: ", repr(datetime.fromisoformat(iso_8601_str)))
+print("Custom: ", datetime.strptime(iso_8601_str, '%Y-%m-%dT%H:%M:%S%z'))
 
 # ~ datetime to str ~
 print("====== datetime to str ======")
