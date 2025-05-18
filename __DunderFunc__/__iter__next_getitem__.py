4f073a0d5883399410aad14b055cda16098e0fa2
lijunqi <lijunqi001@163.com>
2025-05-18 21:27:26 +0800

    userlist

--- __DunderFunc__/__iter__next_getitem__.py
+++ __DunderFunc__/__iter__next_getitem__.py
@@ -1,25 +1,22 @@
 """
 Python迭代器的基本方法: iter(), next()
 
-迭代器：
-    1. 迭代器是一个可以记住遍历的位置的对象. 它能在你调用next()方法时返回容器中的下一个值
-    2. 迭代器对象从集合的第一个元素开始访问，直到所有的元素被访问完结束. 迭代器只能往前不会后退.
-    一个迭代器需要同时实现 __iter__() 和 __next__()
-        __iter__(): 返回自身以获取该类的迭代器
-        __next__(): 返回容器中的下一个元素, 当数据取完时, 要引发一个StopIteration异常
+* 可迭代对象(Iterable): 实现了__iter__()方法的对象
+    1. 是能够一次性返回其成员的对象.
+    2. 常见Iterable包括所有的序列类型(如list,str和tuple)以及一些非序列类型,
+        如dict和文件, 以及任何定义了__iter__()方法或__getitem__()的类的对象
+        (可以把自己的成员一个一个返回(或者说遍历自己的成员)的一类对象).
 
-可迭代对象(Iterable)是能够一次性返回其成员的对象.
-常见Iterable包括所有的序列类型(如list,str和tuple)以及一些非序列类型,
-如dict和文件, 以及任何定义了__iter__()方法或__getitem__()的类的对象
-(可以把自己的成员一个一个返回(或者说遍历自己的成员)的一类对象).
+* * 迭代器(Iterator): 实现了__iter()__ 和 __next()__方法的对象
+        1. 迭代器是一个可以记住遍历的位置的对象. 它能在你调用next()方法时返回容器中的下一个值
+        2. 迭代器对象从集合的第一个元素开始访问，直到所有的元素被访问完结束. 迭代器只能往前不会后退.
+        3. 一个迭代器需要同时实现 __iter__() 和 __next__()
+            __iter__(): 返回自身以获取该类的迭代器
+            __next__(): 返回容器中的下一个元素, 当数据取完时, 要引发一个StopIteration异常
+        4. 在迭代器中, __iter__ 和 __next__ 是必须的, 而 __init__ 不是
 
 迭代器与列表的区别在于, 构建迭代器的时候, 不像列表把所有元素一次性加载到内存,
 而是以一种延迟计算(lazy evaluation)方式返回元素
-
-在迭代器中, __iter__ 和 __next__ 是必须的, 而 __init__ 不是
-
-在 python 中, 实现了__iter__()方法的对象就是一个可迭代对象(Iterable),
-实现了__iter()__ 和 __next()__方法的对象就是一个迭代器(Iterator)
 
 iter(Iterable) return an Iterator
 next(Iterator) return lazily produce next value
@@ -114,11 +111,17 @@ class MyList(list):
         return self._lst.__iter__()
 
     def __next__(self):
+        # next() function.
         print('__next__ called')
-        return self._lst.__next__()
+        return next(self._lst)
+
+    def __len__(self):
+        # len() function.
+        print('__len__ called')
+        return len(self._lst)
 
     def __getitem__(self, index):
-        # [] operator
+        # [] operator. Sequential indexing: x[0], x[1],...
         print('__getitem__ called')
         return self._lst[index]
 
@@ -131,6 +134,79 @@ print(f"{my_lst[1] = }")
 print(f"{my_lst[2] = }")
 
 
+print("====================== StringList =====================")
+class StringList(list):
+    def __init__(self, iterable):
+        super().__init__(str(item) for item in iterable)
+
+    def __setitem__(self, index, item):
+        super().__setitem__(index, str(item))
+
+    def insert(self, index, item):
+        super().insert(index, str(item))
+
+    def append(self, item):
+        super().append(str(item))
+
+    def extend(self, other):
+        if isinstance(other, type(self)):
+            super().extend(other)
+        else:
+            super().extend(str(item) for item in other)
+
+
+my_str_list = StringList(['a', 'b', 'c'])
+for j in my_str_list:
+    print(f"j = {j}")
+
+print(f"{my_str_list[0] = }")
+print(f"{my_str_list[1] = }")
+print(f"{my_str_list[2] = }")
+print(f"len = {len(my_str_list)}")
+
+
+print("====================== MyUserList =====================")
+from collections import UserList
+
+class MyUserList(UserList):
+    #def __init__(self, iterable):
+    #    super().__init__(iterable)
+
+    #def __setitem__(self, index, item):
+    #    self.data[index] = str(item)
+
+    #def insert(self, index, item):
+    #    self.data.insert(index, str(item))
+
+    #def append(self, item):
+    #    self.data.append(str(item))
+
+    #def extend(self, other):
+    #    if isinstance(other, type(self)):
+    #        self.data.extend(other)
+    #    else:
+    #        self.data.extend(str(item) for item in other)
+
+    def greeting(self):
+        print("### Greeting ###")
+        for u in self.data:
+            print(f"Hello {u}!")
+
+my_user_list = MyUserList(['Tom', 'Jerry', 'Mike'])
+for u in my_user_list:
+    print(f"user = {u}")
+
+print(f"{my_user_list[0] = }")
+print(f"{my_user_list[1] = }")
+print(f"{my_user_list[2] = }")
+print(f"len1 = {len(my_user_list)}")
+my_user_list.greeting()
+my_user_list.append("Lucy")
+print(f"len2 = {len(my_user_list)}")
+my_user_list.greeting()
+
+
+print("====================== __getitem__ =====================")
 class Person:
     def __getitem__(self, val):
         return f'__getitem__({val}) called.'
