fecb7ffc7deb7b725ba840f81c0db9907cf8a232
lijunqi <lijunqi001@163.com>
2025-05-21 22:58:34 +0800

    update

--- lib_pytest/mytests/conftest.py
+++ lib_pytest/mytests/conftest.py
@@ -4,3 +4,10 @@ import pytest
 def my_fixture():
     print("This is my fixture.")
     return 123
+
+#@pytest.fixture(scope="session") # only "teardown" once
+@pytest.fixture(scope="function")
+def fixture_with_teardown():
+    print("This is second fixture.")
+    yield 456
+    print("======> Teardown second fixture.")
--- lib_pytest/mytests/engine/test_engine.py
+++ lib_pytest/mytests/engine/test_engine.py
@@ -8,6 +8,25 @@ def test_engine_start(my_fixture):
 
 
 @mark.engine
-def test_engine_stop():
-    print("Engine is stopped.")
+def test_engine_running(fixture_with_teardown):
+    print("Engine is running:", fixture_with_teardown)
     assert True
+
+
+@mark.engine
+def test_engine_stop(fixture_with_teardown):
+    print("Engine is stopped:", fixture_with_teardown)
+    a = 123
+    b = 123
+    assert a == b
+
+
+@mark.engine
+def test_always_fail():
+    a = 123
+    b = {"name": "Tom"}
+    c = '123'
+    print("This case should be failed(1).")
+    assert a == c
+    assert a == b
+    print("This case should be failed(2).")
