3fc9f8048f85ca0aaf0ca94638ec3648b68a69a5
lijunqi <lijunqi001@163.com>
2025-06-24 21:07:33 +0800

    singleton and fastapi==0.115.13

--- __DunderFunc__/__new__.py
+++ __DunderFunc__/__new__.py
@@ -5,7 +5,8 @@ class Singleton(object):
     
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            #cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super().__new__(cls, *args, **kwargs)
         return cls._instance
 
 
@@ -16,6 +17,7 @@ print(("s1 is s2: %s") % (s1 is s2))
 
 class A(object):
     def __new__(cls, *args, **kwargs):
+        print("cls is", cls.__name__)
         print("Enter A's __new__()")
         print("Leave A's __new__()")
         # * Must have a return value
@@ -35,7 +37,7 @@ a()
 class B():
     def __new__(cls, *args, **kwargs):
         func, *params = args
-        print('cls: ', cls)
+        print('cls: ', cls.__name__)
         print('func: ', func)
         print('params: ', params)
 
@@ -47,3 +49,48 @@ def foo():
     print('foo')
 
 b = B(foo, 1, 2, 3)
+
+
+# * Order of __new__ and __init__
+print("========= Metaclass =========")
+class Parent(type):
+    def __new__(cls, *args, **kwargs):
+        print("This is Parent __new__")
+        return super().__new__(cls, *args, **kwargs)
+    
+    def __init__(cls, name, bases, dct):
+        print("This is Parent __init__")
+        super().__init__(name, bases, dct)
+
+class Child(metaclass=Parent):
+    def __new__(cls):
+        print("This is Child __new__")
+        return super().__new__(cls)
+    
+    def __init__(self):
+        print("This is Child __init__")
+        return super().__init__()
+
+c1 = Child()
+
+
+print("========= Normal class =========")
+class Parent2(object):
+    def __new__(cls, *args, **kwargs):
+        print("This is Parent2 __new__")
+        return super().__new__(cls, *args, **kwargs)
+    
+    def __init__(self):
+        print("This is Parent2 __init__")
+        super().__init__()
+
+class Child2(Parent2):
+    def __new__(cls):
+        print("This is Child2 __new__")
+        return super().__new__(cls)
+    
+    def __init__(self):
+        print("This is Child2 __init__")
+        return super().__init__()
+
+c2 = Child2()
