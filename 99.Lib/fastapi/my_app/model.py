35fff929c84baac018b3670fb92d9db85f1a9e2b
Jacky Junqi Li <jli21@rockwellautomation.com>
2025-06-26 14:37:57 +0800

    update

--- 99.Lib/fastapi/my_app/model.py
+++ 99.Lib/fastapi/my_app/model.py
@@ -1,8 +1,13 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class MyItem(BaseModel):
     name: str
-    description: str | None = None
+    description: Optional[str] = Field(
+        default=None,
+        description="An optional description of the item",
+    )
+    #description: str | None = None
     price: float = Field(description="Price is required!!!")
     tax: float = Field(default=1.23)
--- 99.Lib/fastapi/single_send_my_item.py
+++ 99.Lib/fastapi/single_send_my_item.py
@@ -3,7 +3,7 @@ from utils import URL, send_get, send_post
 if __name__ == "__main__":
     payload = {
         "name": "Tom",
-        "description": "This is Tom",
-        #"price": 1.0,
+        #"description": "This is Tom",
+        "price": 1.0,
     }
     send_post(f"{URL}/my_item/", payload)
