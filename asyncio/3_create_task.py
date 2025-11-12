b37b6ca91806e5e402f4fd42ca75aa12850115ac
Jacky <jli21@ra.rockwell.com>
2025-11-12 10:54:33 +0800

    async

--- asyncio/3_create_task.py
+++ asyncio/3_create_task.py
@@ -4,26 +4,27 @@ import time
 # The asyncio.create_task() function to run coroutines concurrently
 # as asyncio Tasks.
 async def say_after(delay, what):
-#    print(f"[{time.strftime('%X')}] {delay}, {what}")
+    print(f"[{time.strftime('%X')}]Start {delay}, {what}")
     await asyncio.sleep(delay)
-    print(f"[{time.strftime('%X')}]{what}")
+    #time.sleep(delay) # ! This would block the event loop, cause task2 start after task1 finish
+    print(f"[{time.strftime('%X')}]End {what}")
 
 async def main():
     # "Tasks" are used to schedule coroutines concurrently.
     # When a coroutine is wrapped into a Task with functions like asyncio.create_task() the coroutine is automatically scheduled to run soon
     task1 = asyncio.create_task(
-        say_after(1, 'hello'))
+        say_after(3, 'This is task 1'))
 
     task2 = asyncio.create_task(
-        say_after(2, 'world'))
+        say_after(7, 'This is task 2'))
 
     print(f"[{time.strftime('%X')}]======  Begin sleep")
-#    time.sleep(5)
+    #time.sleep(5)
     await asyncio.sleep(5)
     print(f"[{time.strftime('%X')}]======  After sleep")
 
-    # Wait until both tasks are completed (should take
-    # around 2 seconds.)
+    # Wait until both tasks are completed
+    # (should take around 7 seconds.)
     await task2
     print(f"[{time.strftime('%X')}]======  Here call await.")
     await task1
